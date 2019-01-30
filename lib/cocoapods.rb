b5e5153f546c1c2ae02cf9c10ef1e4711392a24f
Tracy Keeling <tracyk@zillowgroup.com>
2019-01-30 12:59:13 -0800

    Create "Builder" as a copy of "Validator" (so I can track the changes I made)

--- lib/cocoapods.rb
+++ lib/cocoapods.rb
@@ -51,6 +51,7 @@ module Pod
   autoload :Sandbox,                   'cocoapods/sandbox'
   autoload :Target,                    'cocoapods/target'
   autoload :Validator,                 'cocoapods/validator'
+  autoload :Builder,                   'cocoapods/builder'
 
   module Generator
     autoload :Acknowledgements,        'cocoapods/generator/acknowledgements'
